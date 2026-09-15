3f3f3878e6d3230c263a9237fce40c0cb9c9614f
Sam Adams <119465691+SamAdams1@users.noreply.github.com>
2026-09-15 15:54:33 -0400

    add more getters/setters for quiz class

--- testTimeJava/src/Quiz.java
+++ testTimeJava/src/Quiz.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Quiz {
     private String id;
     private String title;
@@ -23,4 +26,51 @@ public class Quiz {
     public List<Question> getQuestions() {
         return questions;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getJoinCode() {
+        return joinCode;
+    }
+
+    public void setJoinCode(String joinCode) {
+        this.joinCode = joinCode;
+    }
+
+    public String getQrCode() {
+        return qrCode;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setQrCode(String qrCode) {
+        this.qrCode = qrCode;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    
+
 }
